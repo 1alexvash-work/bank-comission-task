4516fe45f16afd03c9a825d12a0dad6a9b7852c4
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-30 21:18:45 +0300

    refactor: determineOperation to have less nesting and duplication

--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,17 +1,16 @@
 import { Operation } from "../types";
 
 const determineOperation = (operation: Operation) => {
-  if (operation.type === "cash_in") {
+  if (operation.type === "cash_out" && operation.user_type === "natural") {
+    return "cash_out_natural";
+  } else if (
+    operation.type === "cash_out" &&
+    operation.user_type === "juridical"
+  ) {
+    return "cash_out_juridical";
+  } else {
     return "cash_in";
-  } else if (operation.type === "cash_out") {
-    if (operation.user_type === "natural") {
-      return "cash_out_natural";
-    } else if (operation.user_type === "juridical") {
-      return "cash_out_juridical";
-    }
   }
-
-  return "cash_in";
 };
 
 export { determineOperation };
