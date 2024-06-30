badbe7f31e675e1eb45247cc5c9c413f7ad10bab
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-30 18:01:51 +0300

    feat: add default testing env setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello, beautiful world!");
+import { runTests } from "./tests";
+import inputData from "./input.json";
+import calculateCommissionFees from "./calculateCommissionFees";
+
+const expectedList = [0.06, 0.9, 87.0, 3.0, 0.3, 0.3, 5.0, 0.0, 0.0];
+
+const tests = inputData.map((input, index) => ({
+  actual: calculateCommissionFees(input),
+  expected: expectedList[index],
+}));
+
+const runTestResults = runTests({ tests });
+
+console.log({ runTestResults });
