712e907ff22356a1ae036cf185bc6eb65e96891e
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-30 18:09:45 +0300

    feat: add API folder

---
+++ src/API/index.ts
@@ -0,0 +1,9 @@
+const API = {
+  cashIn: "https://private-00d723-paysera.apiary-mock.com/cash-in",
+  cashOutNatural:
+    "https://private-00d723-paysera.apiary-mock.com/cash-out-natural",
+  cashOutJuridical:
+    "https://private-00d723-paysera.apiary-mock.com/cash-out-juridical",
+};
+
+export default API;
