feffda270de5b5c35e522fc21c86b8dd06ce2f23
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-06-30 18:29:44 +0300

    feat: add determineOperation helper

---
+++ src/helpers/index.ts
@@ -0,0 +1,15 @@
+import { Operation } from "../types";
+
+const determineOperation = (operation: Operation) => {
+  if (operation.type === "cash_in") {
+    return "cash_in";
+  } else if (operation.type === "cash_out") {
+    if (operation.user_type === "natural") {
+      return "cash_out_natural";
+    } else if (operation.user_type === "juridical") {
+      return "cash_out_juridical";
+    }
+  }
+};
+
+export { determineOperation };
