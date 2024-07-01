90d4dfa94adccef376838a407661d968ace9ed2b
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-07-01 11:57:30 +0300

    feat: improve flow for receiving results, to possible avoid receiving data in unordered format

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,42 @@ import inputData from "./input.json";
 import calculateCommissionFees from "./calculateCommissionFees";
 import { Operation } from "./types";
 
-const expectedList = [0.06, 0.9, 87.0, 3.0, 0.3, 0.3, 5.0, 0.0, 0.0];
+const expectedList = [
+  0.06, // first test case result
+  0.9, // second test case result
+  87.0, // third test case result
+  3.0, // fourth test case result
+  0.3, // fifth test case result
+  0.3, // sixth test case result
+  5.0, // seventh test case result
+  0.0, // eighth test case result
+  0.0, // ninth test case result
+];
 
-const runAsyncTests = async () => {
-  console.time("⌛ Test time");
+const getResults = async () => {
+  console.time("⌛ time to get results");
 
-  const tests = await Promise.all(
-    inputData.map(async (operation, index) => ({
-      actual: await calculateCommissionFees(operation as Operation),
-      expected: expectedList[index],
-    }))
+  const results = await Promise.all(
+    inputData.map(async (operation) => {
+      return await calculateCommissionFees(operation as Operation);
+    })
   );
 
-  const runTestResults = runTests({ tests });
+  console.timeEnd("⌛ time to get results");
+
+  return results;
+};
+
+const runAsyncTests = async () => {
+  const results = await getResults();
+  console.log("results:", results);
+
+  const runTestResults = runTests({
+    tests: results.map((result, index) => ({
+      actual: result,
+      expected: expectedList[index],
+    })),
+  });
 
   console.log({ runTestResults });
 
